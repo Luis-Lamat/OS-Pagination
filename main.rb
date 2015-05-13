c0ba6ef8ac88800289fe007427d1febea5dd0a1c
Pinelo <pinelo93@gmail.com>
2015-05-13 14:36:35 -0500

    Se validan y manda las acciones a la clase administrador

--- main.rb
+++ main.rb
@@ -18,6 +18,7 @@ require_relative "administrador"
 # hash que tiene las acciones permitidas y como valor el tamaño maximo
 # de su input, es decir, de sus caracteres de entrada
 @acciones = {"P" => 3, "A" => 4, "L" => 2, "F" => 1, "E" => 1}
+@metodos = {"P" => "poner_en_memoria", "A" => "asignar", "L" => "borrar", "F" => "fin", "E" => "exit"}
 
 def get_input
   print "Ingrese los datos (Accion tamaño id): "
@@ -31,8 +32,22 @@ end
 def parse_action(input)
   input = input.split
   return false unless @acciones.include? input[0]
-  return false unless @acciones[input[0]] == input.size 
-  true
+  return false unless @acciones[input[0]] == input.size
+  accion = @metodos[input[0]]
+  parametros = Hash.new   
+  case input[0]
+    when "P"
+     parametros = {"n" => input[1], "p"=> input[2]}
+    when "A"
+     parametros = {"d" => input[1], "m"=> input[2], "p"=> input[3]}
+    when "L"
+     parametros = {"p"=> input[1]}
+    else
+    puts "Exit"
+  end
+  datos = Hash.new
+  datos[accion] = parametros
+  return datos
 end
 
 #------------------------------------ MAIN ------------------------------------#
@@ -41,12 +56,11 @@ input = get_input
 
 while input != "exit"
 
-  accion = parse_action(input)
-
-  puts accion
-
+  accion = parse_action(input)  
+#  Administrador.send(accion)  
   # este metodo se encarga de ver la disponibilidad de los marcos
   # Administrador.asignar(id, num_marcos, accion)
+  puts accion
 
   input = get_input
 end
