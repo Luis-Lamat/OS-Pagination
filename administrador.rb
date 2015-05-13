5bab62ddd087a695e7a4c9047e143601871d9de7
Pinelo <pinelo93@gmail.com>
2015-05-13 17:37:05 -0500

    TablaDireccionamiento lista

--- administrador.rb
+++ administrador.rb
@@ -1,7 +1,7 @@
 require_relative "memoria_real"
 require_relative "memoria_virtual"
 
-# TODO: doc
+# TODO: doc, page faults
 class Administrador
 
   def self.poner_en_memoria(id_proceso, bytes)
@@ -11,5 +11,17 @@ class Administrador
     # si funciona:
     # => dejarlo ahi y copiarlo a virtual
   end
-
+  
+  def self.accesar(dir_virtual, id_proceso)
+    # localizar donde esta la pagina que contiene
+#    pagina = dir_virtual.to_i / 8
+      direccion = TablaDireccionamiento.localizar(dir_virtual, id_proceso)
+      return direccion unless direccion == -1
+      return TablaDireccionamiento.actualizar(dir_virtual, id_proceso)     
+    # esta en memoria secundaria?
+    #    page fault
+    #    poner_en_memoria
+    
+        
+  end
 end
